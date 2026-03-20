224556d9e2de5390a6bd1eaa0b53cfa429fb8bb6
Jose Esteban Jara Ortiz <jjaraort@emeal.nttdata.com>
2026-03-20 11:33:43 -0500

    Primer commit para mi carrito de compras

---
+++ CapaNegocio/CN_Recursos.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+
+namespace CapaNegocio
+{
+    public class CN_Recursos
+    {
+        public static string GenerarClave() { 
+            string clave = Guid.NewGuid().ToString("N").Substring(0,6);
+            return clave;
+        }
+        public static string ConvertirSha256(string texto)
+        {
+            // Crear objeto SHA256
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Convertir texto a bytes
+                byte[] bytes = Encoding.UTF8.GetBytes(texto);
+
+                // Calcular hash
+                byte[] hash = sha256.ComputeHash(bytes);
+
+                // Convertir hash a string hexadecimal
+                StringBuilder resultado = new StringBuilder();
+                foreach (byte b in hash)
+                {
+                    resultado.Append(b.ToString("x2")); // formato hexadecimal
+                }
+
+                return resultado.ToString();
+            }
+        }
+
+        public static bool EnviarCorreo(string correo,string asunto,string mensaje) {
+            bool resultado = false;
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(correo);
+                mail.From = new MailAddress("jeort420@gmail.com");
+                mail.Subject = asunto;
+                mail.Body = mensaje;
+                mail.IsBodyHtml = true;
+
+                var smtp = new SmtpClient() { 
+                    Credentials = new NetworkCredential("jeort420@gmail.com", "contraseñaGeneradaDesdelaCuenta"),
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                };
+                smtp.Send(mail);
+                resultado = true;
+            }
+            catch (Exception ex) {
+                resultado = false;
+            }
+            return (resultado);
+        }
+    }
+}
